c77433e666f9e6f7e31dea372336e0211447ff89
luca martino <martinolucatomas@gmail.com>
2023-09-26 15:53:27 -0300

    span

--- src/components/ListContainer/ListContainer.jsx
+++ src/components/ListContainer/ListContainer.jsx
@@ -3,7 +3,9 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import "./ListContainer.css";
 const ListContainer = () => {
   return (
-    <div className='carrito'> <ShoppingCartIcon />Productos dentro del carrito :</div>
+    <div className='carrito'> <ShoppingCartIcon />Productos dentro del carrito :
+    <span> 0 </span>
+    </div>
   )
 }
 
